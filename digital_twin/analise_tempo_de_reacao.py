6460ecc5b6daba866bba62e25f16db402d7271c3
Rafael Nakata <rafaelk.nakata@gmail.com>
2022-12-03 22:59:05 -0300

    Adiciona a análise do tempo de reação

---
+++ digital_twin/analise_tempo_de_reacao.py
@@ -0,0 +1,10 @@
+from digital_twin_utils import get_float_data_from_csv
+import matplotlib.pyplot as plt
+
+column_row, data_rows = get_float_data_from_csv("tempo_de_reacao.csv")
+
+plt.title("Gráfico de dispersão tempo de reação X modo")
+plt.xlabel("Modo de jogo")
+plt.ylabel("Tempo de reação")
+plt.plot([data_row[0] for data_row in data_rows], [data_row[1] for data_row in data_rows], 'ro')
+plt.show()
