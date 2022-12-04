f5a6f4dc560192d68ec29ddd4285501f1b6a2536
Rafael Nakata <rafaelk.nakata@gmail.com>
2022-12-03 23:00:00 -0300

    pequena mudança

--- digital_twin/analise_tempo_de_reacao.py
+++ digital_twin/analise_tempo_de_reacao.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 column_row, data_rows = get_float_data_from_csv("tempo_de_reacao.csv")
 
-plt.title("Gráfico de dispersão tempo de reação X modo")
+plt.title("Gráfico de dispersão tempo de reação X Modo de jogo")
 plt.xlabel("Modo de jogo")
 plt.ylabel("Tempo de reação")
 plt.plot([data_row[0] for data_row in data_rows], [data_row[1] for data_row in data_rows], 'ro')
