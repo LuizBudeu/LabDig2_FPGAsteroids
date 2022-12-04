9fd4b16cfe0c1d1ab24f0560209564c7a61a3d12
Rafael Nakata <rafaelk.nakata@gmail.com>
2022-12-03 22:55:54 -0300

    incrementa a análise de pontuação

--- digital_twin/analise_pontuacao.py
+++ digital_twin/analise_pontuacao.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 
 column_row, data_rows = get_integer_data_from_csv("pontuacao.csv")
 
-plt.plot([data_row[0] for data_row in data_rows], [data_row[1] for data_row in data_rows], 'ro')
+plt.title("Gráfico de dispersão pontuação X modo")
 plt.xlabel(column_row[0])
 plt.ylabel(column_row[1])
+plt.plot([data_row[0] for data_row in data_rows], [data_row[1] for data_row in data_rows], 'ro')
 plt.show()
